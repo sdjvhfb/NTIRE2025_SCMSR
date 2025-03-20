d327f0a2032fbc727ae4a88045822a398cb8df0b
Meteora <136979455+sdjvhfb@users.noreply.github.com>
2025-03-20 14:41:15 +0800

    Add files via upload

---
+++ models/team16_SCMSR.py
@@ -0,0 +1,474 @@
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+from einops import rearrange
+# from basicsr.archs.arch_util import flow_warp 
+# from basicsr.utils.registry import ARCH_REGISTRY
+
+def flow_warp(x, flow, interp_mode='bilinear', padding_mode='zeros', align_corners=True):
+    """Warp an image or feature map with optical flow.
+
+    Args:
+        x (Tensor): Tensor with size (n, c, h, w).
+        flow (Tensor): Tensor with size (n, h, w, 2), normal value.
+        interp_mode (str): 'nearest' or 'bilinear'. Default: 'bilinear'.
+        padding_mode (str): 'zeros' or 'border' or 'reflection'.
+            Default: 'zeros'.
+        align_corners (bool): Before pytorch 1.3, the default value is
+            align_corners=True. After pytorch 1.3, the default value is
+            align_corners=False. Here, we use the True as default.
+
+    Returns:
+        Tensor: Warped image or feature map.
+    """
+    assert x.size()[-2:] == flow.size()[1:3]
+    _, _, h, w = x.size()
+    # create mesh grid
+    grid_y, grid_x = torch.meshgrid(torch.arange(0, h).type_as(x), torch.arange(0, w).type_as(x))
+    grid = torch.stack((grid_x, grid_y), 2).float()  # W(x), H(y), 2
+    grid.requires_grad = False
+
+    vgrid = grid + flow
+    # scale grid to [-1,1]
+    vgrid_x = 2.0 * vgrid[:, :, :, 0] / max(w - 1, 1) - 1.0
+    vgrid_y = 2.0 * vgrid[:, :, :, 1] / max(h - 1, 1) - 1.0
+    vgrid_scaled = torch.stack((vgrid_x, vgrid_y), dim=3)
+    output = F.grid_sample(x, vgrid_scaled, mode=interp_mode, padding_mode=padding_mode, align_corners=align_corners)
+
+    # TODO, what if align_corners=False
+    return output
+
+class LayerNorm(nn.Module):
+    r""" LayerNorm that supports two data formats: channels_last (default) or channels_first. 
+    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with 
+    shape (batch_size, height, width, channels) while channels_first corresponds to inputs 
+    with shape (batch_size, channels, height, width).
+    """
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_first"):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError 
+        self.normalized_shape = (normalized_shape, )
+    
+    def forward(self, x):
+        if self.data_format == "channels_last":
+            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim=True)
+            s = (x - u).pow(2).mean(1, keepdim=True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            return x
+
+# class DynamicTanh(nn.Module):
+#     def __init__(self, normalized_shape, channels_last, alpha_init_value=0.5):
+#         super().__init__()
+#         self.normalized_shape = normalized_shape
+#         self.alpha_init_value = alpha_init_value
+#         self.channels_last = channels_last
+
+#         self.alpha = nn.Parameter(torch.ones(1) * alpha_init_value)
+#         self.weight = nn.Parameter(torch.ones(normalized_shape))
+#         self.bias = nn.Parameter(torch.zeros(normalized_shape))
+
+#     def forward(self, x):
+#         x = torch.tanh(self.alpha * x)
+#         if self.channels_last:
+#             x = x * self.weight + self.bias
+#         else:
+#             x = x * self.weight[:, None, None] + self.bias[:, None, None]
+#         return x
+
+def batch_index_select(x, idx):
+    if len(x.size()) == 3:
+        B, N, C = x.size()
+        N_new = idx.size(1)
+        offset = torch.arange(B, dtype=torch.long, device=x.device).view(B, 1) * N
+        idx = idx + offset
+        out = x.reshape(B*N, C)[idx.reshape(-1)].reshape(B, N_new, C)
+        return out
+    elif len(x.size()) == 2:
+        B, N = x.size()
+        N_new = idx.size(1)
+        offset = torch.arange(B, dtype=torch.long, device=x.device).view(B, 1) * N
+        idx = idx + offset
+        out = x.reshape(B*N)[idx.reshape(-1)].reshape(B, N_new)
+        return out
+    else:
+        raise NotImplementedError
+
+def batch_index_fill(x, x1, x2, idx1, idx2):
+    B, N, C = x.size()
+    B, N1, C = x1.size()
+    B, N2, C = x2.size()
+
+    offset = torch.arange(B, dtype=torch.long, device=x.device).view(B, 1)
+    idx1 = idx1 + offset * N
+    idx2 = idx2 + offset * N
+
+    x = x.reshape(B*N, C)
+
+    x[idx1.reshape(-1)] = x1.reshape(B*N1, C)
+    x[idx2.reshape(-1)] = x2.reshape(B*N2, C)
+
+    x = x.reshape(B, N, C)
+    return x
+
+class PredictorLG(nn.Module):
+    """ Importance Score Predictor
+    """
+    def __init__(self, dim, window_size=8, k=4,ratio=0.5):
+        super().__init__()
+
+        self.ratio = ratio
+        self.window_size = window_size
+        cdim = dim + k
+        embed_dim = window_size**2
+        
+        self.in_conv = nn.Sequential(
+            nn.Conv2d(cdim, cdim//4, 1),
+            LayerNorm(cdim//4),
+            nn.LeakyReLU(negative_slope=0.1, inplace=True),
+        )
+
+        self.out_offsets = nn.Sequential(
+            nn.Conv2d(cdim//4, cdim//8, 1),
+            nn.LeakyReLU(negative_slope=0.1, inplace=True),
+            nn.Conv2d(cdim//8, 2, 1),
+        )
+
+        self.out_mask = nn.Sequential(
+            nn.Linear(embed_dim, window_size),
+            nn.LeakyReLU(negative_slope=0.1, inplace=True),
+            nn.Linear(window_size, 2),
+            nn.Softmax(dim=-1)
+        )
+
+        self.out_CA = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(cdim//4, dim, 1),
+            nn.Sigmoid(),
+        )
+
+        self.out_SA = nn.Sequential(
+            nn.Conv2d(cdim//4, 1, 3, 1, 1),
+            nn.Sigmoid(),
+        )        
+
+    def forward(self, input_x, mask=None, ratio=0.5, train_mode=False):
+
+        x = self.in_conv(input_x)
+
+        offsets = self.out_offsets(x)
+        offsets = offsets.tanh().mul(8.0)
+
+        ca = self.out_CA(x)
+        sa = self.out_SA(x)
+        
+        x = torch.mean(x, keepdim=True, dim=1) 
+
+        x = rearrange(x,'b c (h dh) (w dw) -> b (h w) (dh dw c)', dh=self.window_size, dw=self.window_size)
+        B, N, C = x.size()
+
+        pred_score = self.out_mask(x)
+        mask = F.gumbel_softmax(pred_score, hard=True, dim=2)[:, :, 0:1]
+
+        if self.training or train_mode:
+            return mask, offsets, ca, sa
+        else:
+            score = pred_score[:, : , 0]
+            B, N = score.shape
+            r = torch.mean(mask,dim=(0,1))*1.0
+
+            # Add safety check
+            if torch.isnan(r):
+                r = torch.tensor(0.5, device=r.device)  # Replace NaN with default value
+
+            if self.ratio == 1:
+                num_keep_node = N # int(N * r)
+            else:
+                num_keep_node = min(int(N * r * 2 * self.ratio), N)
+            idx = torch.argsort(score, dim=1, descending=True)
+            idx1 = idx[:, :num_keep_node] # hard token
+            idx2 = idx[:, num_keep_node:] # simple token
+            return [idx1, idx2], offsets, ca, sa
+
+
+class CAMixer(nn.Module):
+    def __init__(self, dim, window_size=8, bias=True, is_deformable=True, ratio=0.5):
+        super().__init__()    
+
+        self.dim = dim
+        self.window_size = window_size
+        self.is_deformable = is_deformable
+        self.ratio = ratio
+
+        k = 3
+        d = 2
+
+        self.c1_r = nn.Conv2d(dim, dim, 1, 1, 0, bias = bias)
+        self.c2_r = nn.Conv2d(dim, dim, 1, 1, 0, bias = bias)
+        self.act1 = nn.SiLU(inplace=True)
+        # self.act1 = nn.GELU()
+        self.project_qk = nn.Linear(dim, dim, bias = bias)
+
+        # Conv
+        self.conv_sptial = nn.Sequential(
+            nn.Conv2d(dim, dim, k, padding=k//2, groups=dim),
+            nn.Conv2d(dim, dim, k, stride=1, padding=((k//2)*d), groups=dim, dilation=d))        
+        self.project_out = nn.Conv2d(dim, dim, 1, 1, 0, bias = bias)
+
+        self.act = nn.GELU()
+        # Predictor
+        self.route = PredictorLG(dim,window_size,ratio=ratio)
+
+    def forward(self,x,condition_global=None, mask=None, train_mode=False):
+        N,C,H,W = x.shape
+
+        out1_act = self.act1(self.c1_r(x))
+        v = self.c2_r(out1_act)
+
+        if self.is_deformable:
+            condition_wind = torch.stack(torch.meshgrid(torch.linspace(-1,1,self.window_size),torch.linspace(-1,1,self.window_size)))\
+                    .type_as(x).unsqueeze(0).repeat(N, 1, H//self.window_size, W//self.window_size)
+            
+            if condition_global is None:
+                _condition = torch.cat([v, condition_wind], dim=1)
+            else:
+                _condition = torch.cat([v, condition_global, condition_wind], dim=1)
+
+        mask, offsets, ca, sa = self.route(_condition,ratio=self.ratio,train_mode=train_mode)
+        
+        qk = out1_act + flow_warp(x, offsets.permute(0,2,3,1), interp_mode='bilinear', padding_mode='border') # N, C, H, W
+
+        vs = v*sa # simple
+
+        v  = rearrange(v,'b c (h dh) (w dw) -> b (h w) (dh dw c)', dh=self.window_size, dw=self.window_size)
+        vs = rearrange(vs,'b c (h dh) (w dw) -> b (h w) (dh dw c)', dh=self.window_size, dw=self.window_size)
+        qk = rearrange(qk,'b c (h dh) (w dw) -> b (h w) (dh dw c)', dh=self.window_size, dw=self.window_size)
+        x_ = rearrange(x,'b c (h dh) (w dw) -> b (h w) (dh dw c)', dh=self.window_size, dw=self.window_size)
+
+        if self.training or train_mode:
+            N_ = v.shape[1]
+            v1,v2 = v*mask, vs*(1-mask)
+            qk1 = qk*mask
+
+            x1 = x_*mask
+
+        else:
+            idx1, idx2 = mask
+            _, N_ = idx1.shape
+            v1,v2 = batch_index_select(v,idx1),batch_index_select(vs,idx2)
+            qk1 = batch_index_select(qk,idx1)
+
+            x1 = batch_index_select(x_,idx1)
+
+        v1 = rearrange(v1,'b n (dh dw c) -> (b n) (dh dw) c', n=N_, dh=self.window_size, dw=self.window_size)
+        qk1 = rearrange(qk1,'b n (dh dw c) -> b (n dh dw) c', n=N_, dh=self.window_size, dw=self.window_size)
+        x1 = rearrange(x1,'b n (dh dw c) -> (b n) (dh dw) c', n=N_, dh=self.window_size, dw=self.window_size)
+
+        qk1 = self.project_qk(qk1)
+        qk1 = rearrange(qk1,'b (n dh dw) c -> (b n) (dh dw) c', n=N_, dh=self.window_size, dw=self.window_size)
+        sim_att = torch.sigmoid(qk1) - 0.5
+        f_attn = (v1 + x1) * sim_att
+        f_attn = rearrange(f_attn,'(b n) (dh dw) c -> b n (dh dw c)', 
+            b=N, n=N_, dh=self.window_size, dw=self.window_size)
+
+        if not (self.training or train_mode):
+            attn_out = batch_index_fill(v, f_attn, v2, idx1, idx2)
+        else:
+            attn_out = f_attn + v2
+
+        attn_out = rearrange(
+            attn_out, 'b (h w) (dh dw c) -> b (c) (h dh) (w dw)', 
+            h=H//self.window_size, w=W//self.window_size, dh=self.window_size, dw=self.window_size
+        )
+        
+        out = attn_out
+        out = self.act(self.conv_sptial(out))*ca + out
+        out = self.project_out(out)
+
+        if self.training:
+            return out, torch.mean(mask,dim=1)
+        return out
+
+
+class LightGatedUnit(nn.Module):
+    def __init__(self, dim, bias=False):
+        super().__init__()
+        self.proj = nn.Conv2d(dim, dim*2, kernel_size=1, bias=bias)
+        self.dwconv = nn.Conv2d(dim, dim, kernel_size=3, 
+                                padding=1, groups=dim, bias=bias)
+        self.act = nn.SiLU()
+        # self.act = nn.GELU()
+        # self.proj2 = nn.Conv2d(dim, dim, kernel_size=1, bias=bias)
+        
+    def forward(self, x):
+        res = x
+        projected = self.proj(x)
+        gates, values = projected.chunk(2, dim=1)
+        values = self.dwconv(values) 
+        gates = self.act(gates)
+        out =  values * gates + res
+        # fn = values * gates
+        # out = self.proj2(fn) + res
+        return out
+
+
+class Block(nn.Module):
+    def __init__(self, n_feats, window_size=8, ratio=0.5):
+        super(Block,self).__init__()
+        
+        self.n_feats = n_feats
+        self.norm1 = LayerNorm(n_feats)
+        self.mixer = CAMixer(n_feats,window_size=window_size,ratio=ratio)
+        self.norm2 = LayerNorm(n_feats)
+        self.ffn = LightGatedUnit(n_feats)
+        
+    def forward(self,x,condition_global=None):
+        if self.training:
+            res, decision = self.mixer(x,condition_global)
+            x = self.norm1(x+res)
+            res = self.ffn(x)
+            x = self.norm2(x+res)
+            return x, decision
+        else:
+            res = self.mixer(x,condition_global)
+            x = self.norm1(x+res)
+            res = self.ffn(x)
+            x = self.norm2(x+res)
+            return x 
+
+
+class Group(nn.Module):
+    def __init__(self, n_feats, n_block, window_size=8, ratio=0.5):
+        super(Group, self).__init__()
+        
+        self.n_feats = n_feats
+
+        self.body = nn.ModuleList([Block(n_feats, window_size=window_size, ratio=ratio) for i in range(n_block)])
+        self.body_tail = nn.Conv2d(n_feats, n_feats, 1, 1, 0)
+        
+    def forward(self,x,condition_global=None):
+        decision = []
+        shortcut = x.clone()
+        if self.training:
+            for _, blk in enumerate(self.body):
+                x, mask = blk(x,condition_global)
+                decision.append(mask)
+            x = self.body_tail(x) + shortcut
+            return x, decision
+        else:
+            for _, blk in enumerate(self.body):
+                x = blk(x,condition_global)
+            x = self.body_tail(x) + shortcut
+            return x 
+        
+
+# @ARCH_REGISTRY.register()
+class SCMSR(nn.Module):
+    def __init__(self, n_block=[4,4,6,6], n_group=4, n_colors=3, n_feats=42, scale=4, ratio=0.5, tile=None):
+        super().__init__()
+        self.n_feats = n_feats
+        self.window_sizes = 16
+        self.tile = tile
+        self.ratio = ratio
+
+        self.head = nn.Conv2d(n_colors, n_feats, 3, 1, 1)
+
+        self.global_predictor = nn.Sequential(nn.Conv2d(n_feats, 8, 1, 1, 0, bias=True),
+                                        nn.LeakyReLU(negative_slope=0.1, inplace=True),
+                                        nn.Conv2d(8, 2, 3, 1, 1, bias=True),
+                                        nn.LeakyReLU(negative_slope=0.1, inplace=True))
+
+        self.scale = scale
+        # define body module
+        self.body = nn.ModuleList([Group(n_feats, n_block=n_block[i], window_size=self.window_sizes, ratio=ratio) for i in range(n_group)])
+        self.body_tail = nn.Conv2d(n_feats, n_feats, 3, 1, 1)
+        # define tail module
+        self.tail = nn.Sequential(
+            nn.Conv2d(n_feats, n_colors*(scale**2), 3, 1, 1),
+            nn.PixelShuffle(scale)
+        )
+        
+    def forward_origin(self, x):
+        decision = []
+        H, W = x.shape[2:]
+        x = self.check_image_size(x)
+
+        x = self.head(x)
+        
+        condition_global = self.global_predictor(x)
+        shortcut = x.clone() 
+        if self.training:
+            for _, blk in enumerate(self.body):
+                x, mask = blk(x,condition_global)
+                decision.extend(mask)
+        else:
+            for _, blk in enumerate(self.body):
+                x = blk(x,condition_global)  
+                 
+        x = self.body_tail(x)
+        x = x + shortcut
+        x = self.tail(x)
+
+        if self.training:
+            return x[:, :, 0:H*self.scale, 0:W*self.scale],  2*self.ratio*(torch.mean(torch.cat(decision,dim=1),dim=(0,1))-0.5)**2 
+        else:
+            return x[:, :, 0:H*self.scale, 0:W*self.scale]
+
+    def forward(self, img_lq, tile=None):
+        tile = self.tile
+        if tile is None or self.training:
+            # test the image as a whole
+            output = self.forward_origin(img_lq)
+        else:
+            # test the image tile by tile or use TileModel
+            b, c, h, w = img_lq.size()
+            tile = min(tile, h, w)
+            tile_overlap = tile//16
+            sf = self.scale
+
+            stride = tile - tile_overlap
+            h_idx_list = list(range(0, h-tile, stride)) + [h-tile]
+            w_idx_list = list(range(0, w-tile, stride)) + [w-tile]
+            E = torch.zeros(b, c, h*sf, w*sf).type_as(img_lq)
+            W = torch.zeros_like(E)
+
+            for h_idx in h_idx_list:
+                for w_idx in w_idx_list:
+                    in_patch = img_lq[..., h_idx:h_idx+tile, w_idx:w_idx+tile]
+                    out_patch = self.forward_origin(in_patch)
+                    out_patch_mask = torch.ones_like(out_patch)
+
+                    E[..., h_idx*sf:(h_idx+tile)*sf, w_idx*sf:(w_idx+tile)*sf].add_(out_patch)
+                    W[..., h_idx*sf:(h_idx+tile)*sf, w_idx*sf:(w_idx+tile)*sf].add_(out_patch_mask)
+            output = E.div_(W)
+
+        return output
+
+    def check_image_size(self, x, ):
+        _, _, h, w = x.size()
+        wsize = self.window_sizes
+        # for i in range(1, len(self.window_sizes)):
+        #     wsize = wsize*self.window_sizes[i] // math.gcd(wsize, self.window_sizes[i])
+        mod_pad_h = (wsize - h % wsize) % wsize
+        mod_pad_w = (wsize - w % wsize) % wsize
+        x = F.pad(x, (0, mod_pad_w, 0, mod_pad_h), 'reflect')
+        return x
+
+if __name__ == "__main__":
+    model = SCMSR(n_feats=42).cuda()
+    model.eval()
+    inputs = torch.rand(1, 3, 256, 256).cuda()
+    with torch.no_grad():
+        output = model(inputs)
+    print(f"Output shape: {output.shape}")
---
+++ test_demo_onlyLR.py
@@ -0,0 +1,246 @@
+import os.path
+import logging
+import torch
+import argparse
+import json
+import glob
+
+from pprint import pprint
+from fvcore.nn import FlopCountAnalysis
+from utils.model_summary import get_model_activation, get_model_flops
+from utils import utils_logger
+from utils import utils_image as util
+
+
+def select_model(args, device):
+    # Model ID is assigned according to the order of the submissions.
+    # Different networks are trained with input range of either [0,1] or [0,255]. The range is determined manually.
+    model_id = args.model_id
+    if model_id == 0:
+        # Baseline: The 1st Place of the `Overall Performance`` of the NTIRE 2023 Efficient SR Challenge 
+        # Edge-enhanced Feature Distillation Network for Efficient Super-Resolution
+        # arXiv: https://arxiv.org/pdf/2204.08759
+        # Original Code: https://github.com/icandle/EFDN
+        # Ckpts: EFDN_gv.pth
+        from models.team00_EFDN import EFDN
+        name, data_range = f"{model_id:02}_EFDN_baseline", 1.0
+        model_path = os.path.join('model_zoo', 'team00_EFDN.pth')
+        model = EFDN()
+        model.load_state_dict(torch.load(model_path), strict=True)
+    elif model_id == 16:
+        from models.team16_SCMSR import SCMSR
+        name, data_range = f"{model_id:02}_SCMSR", 1.0 # You can choose either 1.0 or 255.0 based on your own model
+        model_path = os.path.join('model_zoo', 'team16_SCMSR.pth')
+        model = SCMSR()
+        state_dict = torch.load(model_path)
+        model.load_state_dict(state_dict['params_ema'], strict=True)
+    else:
+        raise NotImplementedError(f"Model {model_id} is not implemented.")
+
+    # print(model)
+    model.eval()
+    tile = None
+    for k, v in model.named_parameters():
+        v.requires_grad = False
+    model = model.to(device)
+    return model, name, data_range, tile
+
+
+def select_dataset(data_dir, mode):
+    # inference on the DIV2K_LSDIR_test set
+    if mode == "test":
+        path = [
+            (
+                p.replace("_HR", "_LR").replace(".png", "x4.png"),
+                p
+            ) for p in sorted(glob.glob(os.path.join(data_dir, "DIV2K_LSDIR_test_HR/*.png")))
+        ]
+
+    # inference on the DIV2K_LSDIR_valid set
+    elif mode == "valid":
+        path = [
+            (
+                p.replace("_HR", "_LR").replace(".png", "x4.png"),
+                p
+            ) for p in sorted(glob.glob(os.path.join(data_dir, "DIV2K_LSDIR_valid_HR/*.png")))
+        ]
+    else:
+        raise NotImplementedError(f"{mode} is not implemented in select_dataset")
+    
+    return path
+
+
+def forward(img_lq, model, tile=None, tile_overlap=32, scale=4):
+    if tile is None:
+        # test the image as a whole
+        output = model(img_lq)
+    else:
+        # test the image tile by tile
+        b, c, h, w = img_lq.size()
+        tile = min(tile, h, w)
+        tile_overlap = tile_overlap
+        sf = scale
+
+        stride = tile - tile_overlap
+        h_idx_list = list(range(0, h-tile, stride)) + [h-tile]
+        w_idx_list = list(range(0, w-tile, stride)) + [w-tile]
+        E = torch.zeros(b, c, h*sf, w*sf).type_as(img_lq)
+        W = torch.zeros_like(E)
+
+        for h_idx in h_idx_list:
+            for w_idx in w_idx_list:
+                in_patch = img_lq[..., h_idx:h_idx+tile, w_idx:w_idx+tile]
+                out_patch = model(in_patch)
+                out_patch_mask = torch.ones_like(out_patch)
+
+                E[..., h_idx*sf:(h_idx+tile)*sf, w_idx*sf:(w_idx+tile)*sf].add_(out_patch)
+                W[..., h_idx*sf:(h_idx+tile)*sf, w_idx*sf:(w_idx+tile)*sf].add_(out_patch_mask)
+        output = E.div_(W)
+
+    return output
+
+def run(model, model_name, data_range, tile, logger, device, args, mode="test"):
+
+    sf = 4
+    border = sf
+    results = dict()
+    results[f"{mode}_runtime"] = []
+    if args.ssim:
+        results[f"{mode}_ssim"] = []
+
+    # --------------------------------
+    # dataset path
+    # --------------------------------
+    data_path = sorted(glob.glob(os.path.join(args.data_dir, "DIV2K_LSDIR_test_LR/*.png")))
+    save_path = os.path.join(args.save_dir, model_name, mode)
+    util.mkdir(save_path)
+
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+
+    for img_lr in data_path:
+
+        # --------------------------------
+        # (1) img_lr
+        # --------------------------------
+        img_name, ext = os.path.splitext(os.path.basename(img_lr))
+        img_name = img_name.replace("x4", "")  # 移除文件名中的 x4
+        img_lr = util.imread_uint(img_lr, n_channels=3)
+        img_lr = util.uint2tensor4(img_lr, data_range)
+        img_lr = img_lr.to(device)
+
+        # --------------------------------
+        # (2) img_sr
+        # --------------------------------
+        start.record()
+        img_sr = forward(img_lr, model, tile)
+        end.record()
+        torch.cuda.synchronize()
+        results[f"{mode}_runtime"].append(start.elapsed_time(end))  # milliseconds
+        img_sr = util.tensor2uint(img_sr, data_range)
+
+        # --- Save Restored Images ---
+        util.imsave(img_sr, os.path.join(save_path, img_name + ext))
+
+    results[f"{mode}_memory"] = torch.cuda.max_memory_allocated(torch.cuda.current_device()) / 1024 ** 2
+    results[f"{mode}_ave_runtime"] = sum(results[f"{mode}_runtime"]) / len(results[f"{mode}_runtime"])
+
+    logger.info("{:>16s} : {:<.3f} [M]".format("Max Memory", results[f"{mode}_memory"]))
+    logger.info("------> Average runtime of (test) is : {:.6f} milliseconds".format(results[f"{mode}_ave_runtime"]))
+
+    return results
+
+
+def main(args):
+    utils_logger.logger_info("NTIRE2025-EfficientSR", log_path="NTIRE2025-EfficientSR.log")
+    logger = logging.getLogger("NTIRE2025-EfficientSR")
+    # --------------------------------
+    # basic settings
+    # --------------------------------
+    torch.cuda.current_device()
+    torch.cuda.empty_cache()
+    torch.backends.cudnn.benchmark = False
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    json_dir = os.path.join(os.getcwd(), "results.json")
+    if not os.path.exists(json_dir):
+        results = dict()
+    else:
+        with open(json_dir, "r") as f:
+            results = json.load(f)
+
+    # --------------------------------
+    # load model
+    # --------------------------------
+    model, model_name, data_range, tile = select_model(args, device)
+    logger.info(model_name)
+
+    # if model not in results:
+    if True:
+        # --------------------------------
+        # restore image
+        # --------------------------------
+
+        # inference on the DIV2K_LSDIR_valid set
+        test_results = run(model, model_name, data_range, tile, logger, device, args, mode="test")
+        # record PSNR, runtime
+        results[model_name] = test_results
+
+        # print(f"Current model_name: {model_name}")
+        # print(results.keys())  # 看看 results 里有哪些模型名称
+        # print(results.get(model_name, {}).keys())  # 看看 model_name 对应的内容
+
+
+        input_dim = (3, 256, 256)  # set the input dimension
+        activations, num_conv = get_model_activation(model, input_dim)
+        activations = activations/10**6
+        logger.info("{:>16s} : {:<.4f} [M]".format("#Activations", activations))
+        logger.info("{:>16s} : {:<d}".format("#Conv2d", num_conv))
+
+        # The FLOPs calculation in previous NTIRE_ESR Challenge
+        # flops = get_model_flops(model, input_dim, False)
+        # flops = flops/10**9
+        # logger.info("{:>16s} : {:<.4f} [G]".format("FLOPs", flops))
+
+        # fvcore is used in NTIRE2025_ESR for FLOPs calculation
+        input_fake = torch.rand(1, 3, 256, 256).to(device)
+        flops = FlopCountAnalysis(model, input_fake).total()
+        flops = flops/10**9
+        logger.info("{:>16s} : {:<.4f} [G]".format("FLOPs", flops))
+
+        num_parameters = sum(map(lambda x: x.numel(), model.parameters()))
+        num_parameters = num_parameters/10**6
+        logger.info("{:>16s} : {:<.4f} [M]".format("#Params", num_parameters))
+        results[model_name].update({"activations": activations, "num_conv": num_conv, "flops": flops, "num_parameters": num_parameters})
+
+        with open(json_dir, "w") as f:
+            json.dump(results, f)
+    
+    fmt = "{:20s}\t{:14s}\t{:10s}\t{:10s}\t{:8s}\t{:8s}\t{:8s}\n"
+    s = fmt.format("Model", "Test Time [ms]", "Params [M]", "FLOPs [G]", "Acts [M]", "Mem [M]", "Conv")
+
+    for k, v in results.items():
+        test_time = f"{v['test_ave_runtime']:3.2f}"
+        mem = f"{v['test_memory']:2.2f}"
+        
+        num_param = f"{v['num_parameters']:2.3f}"
+        flops = f"{v['flops']:2.2f}"
+        acts = f"{v['activations']:2.2f}"
+        conv = f"{v['num_conv']:4d}"
+        s += fmt.format(k, test_time, num_param, flops, acts, mem, conv)
+    with open(os.path.join(os.getcwd(), 'results.txt'), "w") as f:
+        f.write(s)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("NTIRE2025-EfficientSR")
+    parser.add_argument("--data_dir", default="../", type=str)
+    parser.add_argument("--save_dir", default="../results", type=str)
+    parser.add_argument("--model_id", default=0, type=int)
+    parser.add_argument("--include_test", action="store_true", help="Inference on the `DIV2K_LSDIR_test` set")
+    parser.add_argument("--ssim", action="store_true", help="Calculate SSIM")
+
+    args = parser.parse_args()
+    pprint(args)
+
+    main(args)
